5aee63cd3c27dc1d6ce60f0950aaf110f35fe82f
Akarsh Gupta <akarshg9669@gmail.com>
2025-09-10 15:29:49 +0530

    initial commit

--- 392. Is Subsequence.java
+++ 392. Is Subsequence.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int i = 0; 
+        int i = 0; // s ke liye pointer hai 
 
         for (int j = 0; j < t.length() && i < s.length(); j++) {
             if (s.charAt(i) == t.charAt(j)) {
