c876e337bbcfee071a274a0d3fc0360753e11605
Akarsh Gupta <akarshg9669@gmail.com>
2025-09-13 18:55:51 +0530

    initial commit

--- 258. Add Digits.java
+++ 258. Add Digits.java
@@ -3,12 +3,12 @@ class Solution {
         while(num>9){
             int sum = 0;
             while(num>0){
-                sum += num/%0;
-                num/10;
+                sum += num%10;
+                num /= 10;
             }
 
-            num = sum 
+            num = sum ;
         }
-        return sum
+        return num;
     }
 }
