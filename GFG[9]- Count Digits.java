0ec7b0e6524800c80518fe0202cb795421cbcbd9
Akarsh Gupta <akarshg9669@gmail.com>
2025-10-10 12:06:34 +0530

    initial commit

---
+++ GFG[9]- Count Digits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countDigits(int n) {
+        // code here
+        int count = 0;
+        
+        
+        
+      
+            while(n>0){
+                count++;
+                n = n/10;
+            }
+            
+        
+        return count ;
+    }
+}
