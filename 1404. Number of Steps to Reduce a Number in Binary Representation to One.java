233c48a895c31f2253670a5abe31f132883c50ab
Akarsh Gupta <akarshg9669@gmail.com>
2025-11-30 00:39:47 +0530

    initial commit

---
+++ 1404. Number of Steps to Reduce a Number in Binary Representation to One.java
@@ -0,0 +1,19 @@
+import java.math.BigInteger;
+
+class Solution {
+    public int numSteps(String s) {
+        BigInteger num = new BigInteger(s, 2);
+        int steps = 0;
+
+        while (!num.equals(BigInteger.ONE)) {
+            if (num.mod(BigInteger.valueOf(2)).equals(BigInteger.ZERO)) {
+                num = num.divide(BigInteger.valueOf(2));
+            } else {
+                num = num.add(BigInteger.ONE);
+            }
+            steps++;
+        }
+
+        return steps;
+    }
+}
