16282c35adad748b10ce60bb83569f923063253b
Akarsh Gupta <akarshg9669@gmail.com>
2025-12-17 20:08:51 +0530

    initial commit

---
+++ 1952. Three Divisors.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isThree(int n) {
+        int x = (int) Math.sqrt(n);
+
+        if(x*x != n) return false;
+        
+        return isPrime(x);
+    }
+
+    private boolean isPrime(int num){
+        if(num < 2) return false;
+        for(int i=2; i*i<=num; i++){
+            if(num % i ==0) return false;
+        }
+        return true;
+    }
+}
