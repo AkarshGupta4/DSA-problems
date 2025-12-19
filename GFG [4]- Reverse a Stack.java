c489239b6c12ec6623820ba85f55f5bdad920bce
Akarsh Gupta <akarshg9669@gmail.com>
2025-12-19 19:52:32 +0530

    initial commit

--- GFG [4]- Reverse a Stack.java
+++ GFG [4]- Reverse a Stack.java
@@ -19,3 +19,46 @@ class Solution {
         }
     }
 }
+
+
+// import java.util.*;
+
+// public class practice {
+
+//     // Insert element at the bottom of stack
+//     public static void insertAtBottom(Stack<Integer> st, int x) {
+//         if (st.isEmpty()) {
+//             st.push(x);
+//             return;
+//         }
+
+//         int top = st.pop();
+//         insertAtBottom(st, x);
+//         st.push(top);
+//     }
+
+//     // Reverse stack using recursion
+//     public static void reverse(Stack<Integer> st) {
+//         if (st.isEmpty()) {
+//             return;
+//         }
+
+//         int top = st.pop();
+//         reverse(st);
+//         insertAtBottom(st, top);
+//     }
+
+//     public static void main(String[] args) {
+//         Stack<Integer> st = new Stack<>();
+
+//         st.push(1);
+//         st.push(2);
+//         st.push(3);
+//         st.push(4);
+//         st.push(5);
+
+//         reverse(st);
+
+//         System.out.println(st);
+//     }
+// }
