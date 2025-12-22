43708d18e6ef25ac0b6f75dd026f54f8119a70ea
Akarsh Gupta <akarshg9669@gmail.com>
2025-12-22 09:59:26 +0530

    3783 pushed

---
+++ 3783. Mirror Distance of an Integer.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int mirrorDistance(int n) {
+        int rev = 0;
+        for(int i=n; i>0 ;i = i/10){
+            rev = rev * 10 + (i % 10);
+        }
+
+        return Math.abs(n - rev);
+    }
+}
